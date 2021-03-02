ab2b2648cb8965c8eb188b01c6b698b65fcda8c8
Rajasekhar Durvasula <rdurvasula@Rajasekhars-MacBook-Pro.local>
2021-03-02 11:27:05 +0530

    add prereqs rhel73

--- scripts/prereqs_rhel73.sh
+++ scripts/prereqs_rhel73.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-MYSQL_ROOT_PASSWORD="l55Wl06g121my7870g3z9092"
-MYSQL_APPUSER_PASSWORD="ek470oJ05dj057465137lh00"
-cat <<EOT > /etc/profile.d/locale.sh
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-EOT
-# overwrite invalid repo entries
-cat scripts/azure_yum_repo.txt > /etc/yum.repos.d/rh-cloud.repo
-yum clean all
-yum -y install java-1.8.0-openjdk-devel maven
+echo "export LC_ALL=\"en_US.UTF-8\"" > /etc/profile.d/locale.sh
+echo "export LC_CTYPE=\"en_US.UTF-8\"" >> /etc/profile.d/locale.sh
+# As per Azure documentation
+yum --disablerepo='*' remove 'rhui-azure-rhel7'
+yum --config='https://rhelimage.blob.core.windows.net/repositories/rhui-microsoft-azure-rhel7-eus.config' install 'rhui-azure-rhel7-eus'
+echo $(. /etc/os-release && echo $VERSION_ID) > /etc/yum/vars/releasever
+yum -y install git java-1.8.0-openjdk-devel maven
 yum install -y mariadb-server mariadb
 systemctl start mariadb
-# set root password
-mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
-systemctl restart mariadb
 # secure mysql
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "update mysql.user set Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') where User='root'"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "delete from mysql.user where User='root' and Host not in ('localhost','127.0.0.1','::1')"
+mysql -u root -p"" -e "update mysql.user set Password=PASSWORD\('${MYSQL_ROOT_PASSWORD}'\) where User='root'"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "delete from mysql.user where User='root' and Host not in \('localhost','127.0.0.1','::1'\)"
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "delete from mysql.user where User=''"
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "delete from mysql.db where Db='test' or Db='test\_%'"
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "flush privileges"
