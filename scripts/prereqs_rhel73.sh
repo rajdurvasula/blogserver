8df26bffdae2bf91869f60bf8551f823234e4078
Rajasekhar Durvasula <rdurvasula@Rajasekhars-MacBook-Pro.local>
2021-03-21 14:47:05 +0530

    install maven 3.6

--- scripts/prereqs_rhel73.sh
+++ scripts/prereqs_rhel73.sh
@@ -11,7 +11,15 @@ echo "export LC_CTYPE=\"en_US.UTF-8\"" >> /etc/profile.d/locale.sh
 yum --disablerepo='*' remove 'rhui-azure-rhel7' -y
 yum --config='https://rhelimage.blob.core.windows.net/repositories/rhui-microsoft-azure-rhel7-eus.config' install 'rhui-azure-rhel7-eus' -y
 echo $(. /etc/os-release && echo $VERSION_ID) > /etc/yum/vars/releasever
-yum install git java-1.8.0-openjdk-devel maven -y
+yum install git java-1.8.0-openjdk-devel -y
+# Install Maven 3.1.0
+wget https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar xzf apache-maven-3.6.3-bin -C /opt
+ln -s /opt/apache-maven-3.6.3-bin /opt/mavena
+cat <<_EOF_ > /etc/profile.d/maven.sh
+export M2_HOME=/opt/maven
+export PATH=$PATH:$M2_HOME/bin
+_EOF_
 yum install mariadb-server mariadb -y
 systemctl start mariadb
 # secure mysql
