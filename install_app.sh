9440cbada1131907c222a02f733cebec1bbcd7fd
Rajasekhar Durvasula <rdurvasula@Rajasekhars-MacBook-Pro.local>
2021-03-21 15:40:14 +0530

    fix maven path

--- install_app.sh
+++ install_app.sh
@@ -2,5 +2,5 @@
 yum -y install git vim
 git clone https://github.com/rajdurvasula/blogserver.git
 cd blogserver
-/opt/maven/bin/mvn -DskipTests clean package
+/opt/maven/bin/mvn clean package -DskipTests 
 /opt/maven/bin/mvn spring-boot:run
