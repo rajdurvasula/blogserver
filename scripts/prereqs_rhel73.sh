fbe0427f76174455baffb53452ef7c3481e654a6
Rajasekhar Durvasula <rdurvasula@Rajasekhars-MacBook-Pro.local>
2021-03-21 15:08:39 +0530

    fix maven install

--- scripts/prereqs_rhel73.sh
+++ scripts/prereqs_rhel73.sh
@@ -15,7 +15,7 @@ yum install git java-1.8.0-openjdk-devel -y
 # Install Maven 3.1.0
 wget https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar xzf apache-maven-3.6.3-bin -C /opt
-ln -s /opt/apache-maven-3.6.3-bin /opt/mavena
+ln -s /opt/apache-maven-3.6.3 /opt/maven
 cat <<_EOF_ > /etc/profile.d/maven.sh
 export M2_HOME=/opt/maven
 export PATH=$PATH:$M2_HOME/bin
