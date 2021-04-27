d999b8d2295bd610ba9cd590e5b2e89910b2e57e
Rajasekhar Durvasula <rdurvasula@Rajasekhars-MacBook-Pro.local>
2021-04-27 16:28:08 +0530

    fix blob script params

--- copy_files_blob.sh
+++ copy_files_blob.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Arguments:
 # 1. Azure Blob Storage Service URL
-/usr/bin/azcopy login --identity
-export AZCOPY_AUTO_LOGIN_TYPE=MSI
-/usr/bin/azcopy copy /var/log/blogserver.log $1
+#/usr/bin/azcopy login --identity
+#export AZCOPY_AUTO_LOGIN_TYPE=MSI
+#/usr/bin/azcopy copy /var/log/blogserver.log $1
+#
+# Arguments:
+# 1. file path
+# 2. Blob Storage Service URL (including SAS Token)
+/usr/bin/azcopy copy $1 $2
