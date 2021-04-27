fbeac52c436136bb419b9c381745aa2bec2b3974
Rajasekhar Durvasula <rdurvasula@Rajasekhars-MacBook-Pro.local>
2021-04-27 19:37:24 +0530

    fix copy script

--- copy_files_blob.sh
+++ copy_files_blob.sh
@@ -8,4 +8,10 @@
 # Arguments:
 # 1. file path
 # 2. Blob Storage Service URL (including SAS Token)
+if [ ! -f /usr/bin/azcopy ]; then
+  curl -L -o azcopy_linux_amd64_10.10.0.tar.gz https://aka.ms/downloadazcopy-v10-linux
+  tar xzf azcopy_linux_amd64_10.10.0.tar.gz
+  chmod +x azcopy
+  cp azcopy /usr/bin/
+fi
 /usr/bin/azcopy copy $1 $2
